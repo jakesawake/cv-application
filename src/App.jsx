fd8ddb3f756995f8807a134986c442b4ac45b390
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-09-01 05:33:37 -0400

    Imported components to main App

--- src/App.jsx
+++ src/App.jsx
@@ -3,16 +3,23 @@ import ContactSection from "./components/ContactSection";
 import ExperienceSection from "./components/ExperienceSection";
 import EducationSection from "./components/EducationSection";
 import SkillsSection from "./components/SkillsSection";
+import "./styles/App.css";
 
 function App() {
   return (
-    <>
-      <IntroSection />
+    <div className="cv">
+      <header className="cv-header">
+        <IntroSection />
+      </header>
+      <main className="cv-main">
+        <ExperienceSection />
+      </main>
       <ContactSection />
-      <ExperienceSection />
-      <EducationSection />
-      <SkillsSection />
-    </>
+      <aside className="cv-sidebar">
+        <SkillsSection />
+        <EducationSection />
+      </aside>
+    </div>
   );
 }
 
