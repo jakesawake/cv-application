264c64131bf25ce2678e1c9c0fddb7acda9663e7
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-09-01 05:29:16 -0400

    Fleshed out ExperienceEntry component

--- src/components/ExperienceEntry.jsx
+++ src/components/ExperienceEntry.jsx
@@ -1,12 +1,108 @@
-export default function ExperienceEntry() {
+import { useState } from "react";
+
+export default function ExperienceEntry({ entry, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(true);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    onChange({ ...entry, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsEditing(false);
+  }
+
   return (
-    <div className="experience-entry">
-      <strong>XBASE Technologies</strong>, <span>Toronto, ON</span>,
-      <p>IT Support Analyst</p>
-      <time dateTime="2025-03">March, 2025</time>
-      <ul>
-        <li>Provided technical support to clients across various industries</li>
-      </ul>
+    <div className="addable-entry">
+      {isEditing ? (
+        <form onSubmit={handleSubmit}>
+          <label>
+            Company name:
+            <input
+              type="text"
+              name="company"
+              value={entry.company}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            City name:
+            <input
+              type="text"
+              name="city"
+              value={entry.city}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Title name:
+            <input
+              type="text"
+              name="title"
+              value={entry.title}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Start date:
+            <input
+              type="text"
+              name="dateFrom"
+              value={entry.dateFrom}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            End date:
+            <input
+              type="text"
+              name="dateTo"
+              value={entry.dateTo}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Duties:
+            <input
+              type="text"
+              name="duties"
+              value={entry.duties}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button type="button" onClick={onDelete(entry.id)}>
+            Remove
+          </button>
+
+          <button type="submit" className="submit-button">
+            Submit
+          </button>
+        </form>
+      ) : (
+        <>
+          <span>
+            <strong>{entry.company}</strong>, {entry.city} - {entry.title}
+          </span>
+          <time className="time-entry" dateTime={entry.dateFrom}>
+            {entry.dateFrom}
+          </time>{" "}
+          to
+          <time className="time-entry" dateTime={entry.dateTo}>
+            {entry.dateTo}
+          </time>
+          <ul>
+            <li>{entry.duties}</li>
+          </ul>
+        </>
+      )}
     </div>
   );
 }
