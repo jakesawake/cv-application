8ee5b2006b210e1de8b4fca32825e5e4b4031652
Jake Canlas <jakechristiancanlas1@gmail.com>
2026-09-03 21:07:36 -0400

    Added submission logic for education applicant entry

--- src/components/EducationEntry.jsx
+++ src/components/EducationEntry.jsx
@@ -1,24 +1,138 @@
-export default function EducationEntry() {
+import { useState } from "react";
+
+export default function EducationEntry({ entry, onChange, onDelete }) {
+  const [isEditing, setIsEditing] = useState(true);
+
+  function handleChange(e) {
+    const { name, value } = e.target;
+
+    onChange({ ...entry, [name]: value });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    setIsEditing(false);
+  }
+
   return (
     <div className="flex flex-col gap-0.5">
-      <span>
-        <strong>George Brown</strong>, Toronto, ON - Social Service Worker
-      </span>
-      <p>
-        <em>from</em>{" "}
-        <time className="italic" dateTime="2019-09">
-          September, 2019
-        </time>{" "}
-        <em>to</em>{" "}
-        <time className="italic" dateTime="2021-05">
-          May, 2021
-        </time>
-      </p>
-      <ul>
-        <li>
-          Completed the "Social Service worker". Two-year program with honors
-        </li>
-      </ul>
+      {isEditing ? (
+        <form className="flex flex-col" onSubmit={handleSubmit}>
+          <label>
+            School name:
+            <input
+              type="text"
+              className="border border-blue-500"
+              name="school"
+              value={entry.school}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            City name:
+            <input
+              type="text"
+              className="border border-blue-500"
+              name="city"
+              value={entry.city}
+              onChange={handleChange}
+            />
+          </label>
+          <label>
+            Program name:{" "}
+            <input
+              type="text"
+              className="border border-blue-500"
+              name="programName"
+              value={entry.programName}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Start date:
+            <input
+              type="text"
+              className="border border-blue-500"
+              name="startDate"
+              value={entry.startDate}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            End date:
+            <input
+              type="text"
+              className="border border-blue-500"
+              name="endDate"
+              value={entry.endDate}
+              onChange={handleChange}
+            />
+          </label>
+
+          <label>
+            Accomplishments:{" "}
+            <input
+              type="text"
+              className="border border-blue-500"
+              name="accomplished"
+              value={entry.accomplished}
+              onChange={handleChange}
+            />
+          </label>
+
+          <button
+            type="button"
+            className="rounded-[5px] border border-blue-500 p-1.5 text-[12px]"
+            onClick={() => {
+              onDelete(entry.id);
+            }}
+          >
+            Remove
+          </button>
+
+          <button
+            type="submit"
+            className="rounded-[5px] border border-blue-500 p-1.5 text-[12px]"
+          >
+            Submit
+          </button>
+        </form>
+      ) : (
+        <>
+          <span>
+            <strong>{entry.school}</strong>, {entry.city} - {entry.programName}
+          </span>
+          <span>
+            <time dateTime={entry.startDate} className="italic">
+              {entry.startDate}
+            </time>{" "}
+            to{" "}
+            <time dateTime={entry.endDate} className="italic">
+              {entry.endDate}
+            </time>
+          </span>
+          <ul>
+            <li>{entry.accomplished}</li>
+          </ul>
+          <button
+            className="rounded-[5px] p-1.5 text-[12px]"
+            type="button"
+            onClick={() => setIsEditing(true)}
+          >
+            Edit
+          </button>
+          <button
+            className="rounded-[5px] p-1.5 text-[12px]"
+            type="button"
+            onClick={() => onDelete(entry.id)}
+          >
+            Remove Entry
+          </button>
+        </>
+      )}
     </div>
   );
 }
